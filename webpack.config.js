a9e1efc339e3063613fa920431a681b3fb0b96b2
Son Do <dominhson580@gmail.com>
2022-08-10 22:35:14 +0700

    Correct some typos

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    extry: './src/index.js',
+    entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
