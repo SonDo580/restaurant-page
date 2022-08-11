2262da02ef68e3fff00e4c9565c12f8c7c4dc4d9
Son Do <dominhson580@gmail.com>
2022-08-11 14:27:52 +0700

    Add a heading and a description

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,19 @@
 const home = document.createElement('div');
+home.classList.add('home');
 
+// BANNER SECTION
 const bannerDiv = document.createElement('div');
 bannerDiv.classList.add('banner');
-bannerDiv.textContent = 'test'
+
+const heading = document.createElement('h1');
+heading.textContent = 'Super Cat\'s Restaurant';
+bannerDiv.appendChild(heading);
+
+const description = document.createElement('p');
+description.textContent = 'We make foods and beverages for cats (and human)!';
+bannerDiv.appendChild(description);
 
 home.appendChild(bannerDiv);
+// END BANNER SECTION
 
 export default home;
