1afe4e4c4674d3e26d5e2ba6698da00a94b2f301
Son Do <dominhson580@gmail.com>
2022-08-11 21:55:31 +0700

    Create some beverages

--- src/components/menu/beverages.js
+++ src/components/menu/beverages.js
@@ -1,8 +1,25 @@
+const beverages = [
+    {
+        name: 'Lemonade',
+        description: 'This is a random description',
+        price: '$20',
+        image: '../../images/lemonade'
+    },
+    {
+        name: 'Honey',
+        description: 'This is a random description',
+        price: '$30',
+        image: '../../images/honey'
+    }
+];
+
 const beveragesDiv = document.createElement('div');
 beveragesDiv.classList.add('beverages');
 
-const subHeading = document.createElement('h2');
+const subHeading = document.createElement('h1');
 subHeading.textContent = 'Beverages';
 beveragesDiv.appendChild(subHeading);
+
+
 
 export default beveragesDiv;
