d1d9611889fcff887a353b60d19c23926ce3798a
Son Do <dominhson580@gmail.com>
2022-08-11 21:38:42 +0700

    Use 'menu' module in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style.css';
 import home from './pages/home';
+import menu from './pages/menu';
 
 const contentDiv = document.querySelector('#content');
 contentDiv.appendChild(home);
+contentDiv.appendChild(menu);
