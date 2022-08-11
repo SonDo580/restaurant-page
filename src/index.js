7b5707277ee7b8a72b30e47000c4ff1ead68ebfc
Son Do <dominhson580@gmail.com>
2022-08-11 22:59:14 +0700

    Add contact page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
 import home from './pages/home';
 import menu from './pages/menu';
+import contact from './pages/contact';
 
 const contentDiv = document.querySelector('#content');
 contentDiv.appendChild(home);
 contentDiv.appendChild(menu);
+contentDiv.appendChild(contact);
