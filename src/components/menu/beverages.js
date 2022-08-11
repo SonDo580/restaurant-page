220b432a4d5d189a1377f9f2ec1439e55ed61887
Son Do <dominhson580@gmail.com>
2022-08-11 21:46:25 +0700

    Create beverages section of menu page

---
+++ src/components/menu/beverages.js
@@ -0,0 +1,8 @@
+const beveragesDiv = document.createElement('div');
+beveragesDiv.classList.add('beverages');
+
+const subHeading = document.createElement('h2');
+subHeading.textContent = 'Beverages';
+beveragesDiv.appendChild(subHeading);
+
+export default beveragesDiv;
