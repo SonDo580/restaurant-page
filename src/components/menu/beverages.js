1a75da8eedbbf9eadd6dbdafb05d53bd6cf0be48
Son Do <dominhson580@gmail.com>
2022-08-11 22:21:58 +0700

    Display the beverages in card template

--- src/components/menu/beverages.js
+++ src/components/menu/beverages.js
@@ -3,13 +3,13 @@ const beverages = [
         name: 'Lemonade',
         description: 'This is a random description',
         price: '$20',
-        image: '../../images/lemonade'
+        image: '../../images/lemonade.jpeg'
     },
     {
         name: 'Honey',
         description: 'This is a random description',
         price: '$30',
-        image: '../../images/honey'
+        image: '../../images/honey.jpeg'
     }
 ];
 
@@ -20,6 +20,27 @@ const subHeading = document.createElement('h1');
 subHeading.textContent = 'Beverages';
 beveragesDiv.appendChild(subHeading);
 
+for (let beverage of beverages) {
+    const cardMenu = document.createElement('div');
+    cardMenu.classList.add('card-menu');
 
+    const cardImage = document.createElement('img');
+    cardImage.setAttribute('src', beverage.image);
+    cardMenu.appendChild(cardImage);
+
+    const cardName = document.createElement('h3');
+    cardName.textContent = beverage.name;
+    cardMenu.appendChild(cardName);
+
+    const cardDescription = document.createElement('p');
+    cardDescription.textContent = beverage.description;
+    cardMenu.appendChild(cardDescription);
+
+    const cardPrice = document.createElement('span');
+    cardPrice.textContent = beverage.price;
+    cardMenu.appendChild(cardPrice);
+
+    beveragesDiv.appendChild(cardMenu);
+}
 
 export default beveragesDiv;
