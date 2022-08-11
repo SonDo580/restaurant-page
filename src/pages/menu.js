6013fb0f3db67dc2e30fedbac565a133f1f2e3b8
Son Do <dominhson580@gmail.com>
2022-08-11 22:52:13 +0700

    Add the foods section to menu page

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,4 +1,5 @@
 import beverages from "../components/menu/beverages";
+import foods from "../components/menu/foods";
 
 const menu = document.createElement('div');
 menu.classList.add('menu');
@@ -11,5 +12,10 @@ const subHeading1 = document.createElement('h2');
 subHeading1.textContent = 'Beverages';
 menu.appendChild(subHeading1);
 menu.appendChild(beverages);
+
+const subHeading2 = document.createElement('h2');
+subHeading2.textContent = 'Main Foods';
+menu.appendChild(subHeading2);
+menu.appendChild(foods);
 
 export default menu;
