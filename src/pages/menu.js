8bf70e39847695f3df0e5df3fb5fbd92d8daad1c
Son Do <dominhson580@gmail.com>
2022-08-11 21:37:59 +0700

    Create menu page

---
+++ src/pages/menu.js
@@ -0,0 +1,7 @@
+const menu = document.createElement('div');
+menu.classList.add('menu');
+
+const mainHeading = document.createElement('h1');
+mainHeading.textContent = 'Menu';
+
+export default menu;
