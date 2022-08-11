ffbdde677817b998ae6703a130bec9d33a47d6ad
Son Do <dominhson580@gmail.com>
2022-08-11 17:12:23 +0700

    Add location section to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import banner from '../components/home/banner';
 import review from '../components/home/review';
 import openTime from '../components/home/time';
+import location from '../components/home/location';
 
 const home = document.createElement('div');
 home.classList.add('home');
@@ -8,5 +9,6 @@ home.classList.add('home');
 home.appendChild(banner);
 home.appendChild(review);
 home.appendChild(openTime);
+home.appendChild(location);
 
 export default home;
