b6a62e26910edac2cf0a9d653b28ed7c8d799697
Son Do <dominhson580@gmail.com>
2022-08-11 21:27:51 +0700

    Create the location section of home page

--- src/components/home/location.js
+++ src/components/home/location.js
@@ -8,9 +8,14 @@ heading.textContent = 'Location';
 locationDiv.appendChild(heading);
 
 const locationBox = document.createElement('div');
-const locationImage = document.createElement('img');;
+
+const locationImage = document.createElement('img');
 locationImage.setAttribute('src', locationImageSrc);
 locationBox.appendChild(locationImage);
+
+const locationText = document.createElement('p');
+locationText.textContent = '123 No Street, Sahara, Africa';
+locationBox.appendChild(locationText);
 
 locationDiv.appendChild(locationBox);
 
