3b0ee79145ff2656d3e962a02049eaac9a3b0a50
Son Do <dominhson580@gmail.com>
2022-08-11 21:48:46 +0700

    Add beverage section to menu page

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,7 +1,12 @@
+import beverages from "../components/menu/beverages";
+
 const menu = document.createElement('div');
 menu.classList.add('menu');
 
 const mainHeading = document.createElement('h1');
 mainHeading.textContent = 'Menu';
+menu.appendChild(mainHeading);
+
+menu.appendChild(beverages);
 
 export default menu;
