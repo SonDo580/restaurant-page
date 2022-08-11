52b02cd9edf3a230541de8754d572dfea436baf4
Son Do <dominhson580@gmail.com>
2022-08-11 16:45:07 +0700

    Add openTime section to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import banner from '../components/home/banner';
-import review from '../components/home/review'
+import review from '../components/home/review';
+import openTime from '../components/home/time';
 
 const home = document.createElement('div');
 home.classList.add('home');
 
 home.appendChild(banner);
 home.appendChild(review);
+home.appendChild(openTime);
 
 export default home;
