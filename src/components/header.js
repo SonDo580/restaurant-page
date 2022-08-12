e692c9fe220dd6b6d732b7e99ca6d4eb1550052f
Son Do <dominhson580@gmail.com>
2022-08-12 08:54:16 +0700

    Create a header with buttons to change page

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+const header = document.createElement('header');
+
+const homeButton = document.createElement('button');
+homeButton.textContent = 'Home';
+homeButton.addEventListener('click', showHome);
+header.appendChild(homeButton);
+
+const menuButton = document.createElement('button');
+menuButton.textContent = 'menu';
+menuButton.addEventListener('click', showMenu);
+header.appendChild(menuButton);
+
+const contactButton = document.createElement('button');
+contactButton.textContent = 'contact';
+contactButton.addEventListener('click', showContact);
+header.appendChild(contactButton);
+
+export default header;
