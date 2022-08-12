ed363562df328aff3368bd6bb3b4a752f93d434a
Son Do <dominhson580@gmail.com>
2022-08-12 09:03:54 +0700

    Show Home page initially

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import './style.css';
 import home from './pages/home';
-import menu from './pages/menu';
-import contact from './pages/contact';
 
 const contentDiv = document.querySelector('#content');
 contentDiv.appendChild(home);
-contentDiv.appendChild(menu);
-contentDiv.appendChild(contact);
