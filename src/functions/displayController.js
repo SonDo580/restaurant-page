37438a597360b7d966ad9d98c57076f74ab8b943
Son Do <dominhson580@gmail.com>
2022-08-12 09:02:54 +0700

    Erase the content div before rendering page

--- src/functions/displayController.js
+++ src/functions/displayController.js
@@ -1,14 +1,17 @@
 const contentDiv = document.querySelector('#content');
 
 function showHome() {
+    contentDiv.textContent = '';
     contentDiv.appendChild(home);
 }
 
 function showMenu() {
+    contentDiv.textContent = '';
     contentDiv.appendChild(menu);
 }
 
 function showContact() {
+    contentDiv.textContent = '';
     contentDiv.appendChild(contact);
 }
 
