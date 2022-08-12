769f79b31f143797df9091c7389cb119016964b4
Son Do <dominhson580@gmail.com>
2022-08-12 09:47:17 +0700

    Remove the show active logic

--- src/functions/displayController.js
+++ src/functions/displayController.js
@@ -3,7 +3,6 @@ import menu from '../pages/menu';
 import contact from '../pages/contact';
 
 const contentDiv = document.querySelector('#content');
-const buttons = document.querySelectorAll('header button');
 
 function showHome() {
     showActive('home');
@@ -21,17 +20,6 @@ function showContact() {
     showActive('contact');
     contentDiv.textContent = '';
     contentDiv.appendChild(contact);
-}
-
-function showActive(tab) {
-    for (const button of buttons) {
-        if (button.textContent.toLowerCase() !== tab) {
-            button.classList.remove('active');
-            continue;
-        }
-
-        button.classList.add('active');
-    }
 }
 
 export { showHome, showMenu, showContact };
