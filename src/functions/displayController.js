1acf72f2526613c67a6b1263393d8814d71e546f
Son Do <dominhson580@gmail.com>
2022-08-12 09:42:30 +0700

    Add logic to show active tab

--- src/functions/displayController.js
+++ src/functions/displayController.js
@@ -3,20 +3,35 @@ import menu from '../pages/menu';
 import contact from '../pages/contact';
 
 const contentDiv = document.querySelector('#content');
+const buttons = document.querySelectorAll('header button');
 
 function showHome() {
+    showActive('home');
     contentDiv.textContent = '';
     contentDiv.appendChild(home);
 }
 
 function showMenu() {
+    showActive('menu');
     contentDiv.textContent = '';
     contentDiv.appendChild(menu);
 }
 
 function showContact() {
+    showActive('contact');
     contentDiv.textContent = '';
     contentDiv.appendChild(contact);
+}
+
+function showActive(tab) {
+    for (const button of buttons) {
+        if (button.textContent.toLowerCase() !== tab) {
+            button.classList.remove('active');
+            continue;
+        }
+
+        button.classList.add('active');
+    }
 }
 
 export { showHome, showMenu, showContact };
